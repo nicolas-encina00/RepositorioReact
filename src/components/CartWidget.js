8456d7ff6235d5f0770bcac439e8a5c0ae482a55
nicolas-encina00 <85587894+nicolas-encina00@users.noreply.github.com>
2021-10-21 21:21:58 -0300

    Add files via upload

---
+++ src/components/CartWidget.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const CartWidger = () => {
+    return(
+        <div>
+            <img style={{width: '3rem', height: '3rem', padding: '10px'}} src="../../../logo192.png" alt="logo" className="logo"/>
+        </div>
+    )
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () =>{
     return(
     <header className="header">
         <nav className="navbar"> 
-        <img src="../../../logo192.png" alt="logo" className="logo"/>
+        
         <h1 className="item-nav">titulo</h1>
         <h1 className="item-nav">productos</h1>
         <h1 className="item-nav">complementos</h1>
